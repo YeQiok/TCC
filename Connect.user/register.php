08048e9fc99d894636439b2f15cbe6dfecb64e60
Luis Felipe Marins Batista <felipe.marins.batista2003@gmail.com>
2020-09-26 17:02:00 -0300

    Registro do Usuario

--- Connect.user/register.php
+++ Connect.user/register.php
@@ -47,5 +47,69 @@ if($_SERVER["Metodo_Solucao"] == "POST"){
             $confirmar_senhaerro = "A senha não confere.";
         }
     }
+
+    //checkar se tem erro
+    if(empty($usuarioerro) && empty($senhaerro) && empty($confirmar_senhaerro)){
+
+        $sql = "INSERT INTO users (usuario, senha) VALUES (?,?)";
+
+        if($stmt = mysqli_prepare($connect, $sql)){
+            //Vincular as variáveis à instrução preparada como parâmetros
+            mysqli_stmt_bind_param($stmt, "ss", $param_usuario, $param_senha);
+
+            //Setar parametros
+            $param_usuario = $usuario;
+            $param_senha = password_hash($senha, PASSWORD_DEFAULT);
+
+            //Criar senha encrypt
+
+            if(mysqli_stmt_execute($stmt)){
+                header("location: login.php");
+            }else{
+                echo "Algo deu errado. Por favor, tente novamente mais tarde.";
+            }
+            mysqli_stmt_close($stmt);
+        }
+    }
+    mysqli_close($connect);
 }
 ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-default" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
