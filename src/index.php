fe0eb7dff581cb4b7a2d83099628f1e42ae73540
Luis Felipe Marins Batista <felipe.marins.batista2003@gmail.com>
2020-09-27 17:15:45 -0300

    Index

--- src/index.php
+++ src/index.php
@@ -1,44 +1,276 @@
 <!doctype html>
 <html lang="pt-br">
   <head>
-    <title>Tortas Salgadas & Doces</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="initial-scale=1, width=device-width">
+
+          <title>Tortas Doces - Loja Virtual Dolce Gusto</title>
+
   </head>
-  <body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+  <body class="home">
+    <header class="header">
+        <nav class="navbar navbar-default navbar-loja navbar-static-top hidden-xs" role="navigation">
         <div class="container">
-          <a class="navbar-brand" href="#">
-            <img src="img/logo.png" width="" height="30" alt="Logo">
-          </a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-      
-          <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav ">
-              <li class="nav-item active">
-                <a class="nav-link" href="#">Produtos</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Contato</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Endereço</a>
-              </li>
-            </ul>
+          <div class="row">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+            </div>
+            <div class="collapse navbar-collapse">
+              <div class="col-sm-3 form">
+                <form method="POST" action="#" accept-charset="UTF-8"><input name="_token" type="hidden" value="UCMRZjxWxrXo7AfBX9jO6PK6Lfwy97SQcLxjkN0T">
+                  <input type="hidden" name="_token" value="UCMRZjxWxrXo7AfBX9jO6PK6Lfwy97SQcLxjkN0T">
+                    <div class="input-group">
+                      <input type="text" class="form-control" name="localizar" placeholder="Buscar">
+                      <span class="input-group-btn">
+                      <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
+                      </span>
+                    </div>
+                  </form>
+              </div>
+              <div class="col-sm-5 col-lg-6 links">
+                                  <ul>
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">N&atilde;o sou cadastrado</a></li>
+                  </ul>
+                              </div>
+              <div class="col-md-3 col-sm-4 carrinho_superior">
+                <ul class="nav navbar-nav navbar-nav-cart">
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle carrinho-btn" data-toggle="dropdown" role="button" aria-expanded="false">
+                      <div class="icon"><i class="fa fa-shopping-cart"></i><div class="cartQTD" id="cartQTD"></div></div>
+                      <div class="desc">
+                       <div class="carrinho">Carrinho</div>
+                       <div class="detalhes">Detalhes<span class="caret"></span></div>
+                      </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-cart carrinho-refresh" role="menu">
+                      <li>
+	<span>Total: R$ 0,00</span>
+	<a class="pull-right btn-finalizar" href="/loja-virtual/meu-carrinho">Finalizar Compra</a>
+</li>
+<li class="divider"></li>
+
+<script>
+document.getElementById('cartQTD').innerHTML='0';
+
+</script>                    
+</ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
-      
         </div>
       </nav>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+      <nav class="navbar navbar-default hidden-xs" role="navigation">
+        <div class="container">
+          <div class="row">
+            <div class="navbar-header" onclick="window.location='/loja-virtual';">
+              <div class="navbar-brand navbar-brand-centered">
+                <a href="/loja-virtual">
+                  <img class="img-responsive" src="" width="166">
+                </a>
+              </div>
+            </div>
+            <div class="collapse navbar-collapse">
+              <div class="col-xs-12">
+                <ul class="nav navbar-nav navbar-right nav-loja text-uppercase">
+                  <li><a href="/">Site</a></li>
+                  <li><a href="/#contato">Contato</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <!--
+      MOBILE
+      -->
+      <nav class="visible-xs navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu-navbar-collapse-mobile">
+              <span class="sr-only">Toggle navigation</span>
+              <i class="fa fa-bars"></i>
+            </button>
+            <a href="/loja-virtual/meu-carrinho" class="navbar-toggle">
+              <span class="sr-only">Toggle navigation</span>
+              <div class="icon"><i class="fa fa-shopping-cart"></i></div>
+            </a>
+            <div class="navbar-brand navbar-brand-centered">
+              <a class="ancora" href="/#home-mobile" rel="0">
+                  <img class="img-responsive" src="#" width="66">
+              </a>
+            </div>
+          </div>
+          <div class="collapse navbar-collapse" id="menu-navbar-collapse-mobile">
+            <ul class="nav navbar-nav">
+              <li><a href="/" >Site</a></li>
+              <li><a class="ancora" href="/#contato" rel="62">Contato</a></li>
+              <li><hr></li>
+                                  <li><a href="#">Login</a></li>
+                  <li><a href="#">N&atilde;o sou cadastrado</a></li>
+                            </ul>
+          </div>
+        </div>
+      </nav>
+
+    </header>
+
+    <main class="content">
+
+
+<div class="visible-xs" id="home-mobile">
+    <div class="banner-mobile">
+        <ul>
+            <li class="carousel-home-banner-1">
+                <a class="a-full" href="/loja-virtual/calculadora#ancora"></a>
+            </li>
+            <li class="carousel-home-banner-3">
+                <a class="a-full" href="/loja-virtual/produto/tortas-doces/torta-morango-chocolate"></a>
+            </li>
+            <li class="carousel-home-banner-4"></li>
+        </ul>
+        <a href="#" class="unslider-arrow prev"><i class="fa fa-chevron-left"></i></a>
+        <a href="#" class="unslider-arrow next"><i class="fa fa-chevron-right"></i></a>
+    </div>
+</div>
+
+<div class="visible-sm" id="home-mobile">
+    <div class="banner-mobile">
+        <ul>
+            <li class="carousel-home-banner-1">
+                <a class="a-full" href="/loja-virtual/calculadora#ancora"></a>
+            </li>
+            <li class="carousel-home-banner-3">
+                <a class="a-full" href="/loja-virtual/produto/tortas-doces/torta-morango-chocolate"></a>
+            </li>
+            <li class="carousel-home-banner-4"></li>
+        </ul>
+        <a href="#" class="unslider-arrow prev"><i class="fa fa-chevron-left"></i></a>
+        <a href="#" class="unslider-arrow next"><i class="fa fa-chevron-right"></i></a>
+    </div>
+</div>
+<div class="hidden-xs hidden-sm">
+    <div class="carousel slide carousel_loja" data-ride="carousel" id="myCarousel">
+        <ol class="carousel-indicators">
+                        <li class="active" data-slide-to="0" data-target="#myCarousel">
+                <span class="sr-only">Banner 0</span>
+            </li>
+                        <li class="" data-slide-to="1" data-target="#myCarousel">
+                <span class="sr-only">Banner 1</span>
+            </li>
+                        <li class="" data-slide-to="2" data-target="#myCarousel">
+                <span class="sr-only">Banner 2</span>
+            </li>
+                    </ol>
+        <div class="carousel-inner" role="listbox">
+                        <li class="item b1 active">
+                                <a class="link-banner" href="#"></a>
+                            </li>
+                        <li class="item b2">
+                                <a class="link-banner" href="#"></a>
+                            </li>
+                        <li class="item b3">
+                            </li>
+                    </div>
+    </div>
+</div>
+<div class="listrado" id="ancora" rel="0">
+    <h1>
+        <div class="t2">Monte sua festa</div>
+        <div class="t1">Frete grátis nas compras acima de R$ 350,00</div>
+        <div class="t3">Pague em até 3 vezes s/ juros no cartão</div>
+    </h1>
+</div>
+
+<div class="container">
+    <div class="col-md-3 menu">
+        <h1>Produtos</h1>
+        <ul>
+                        <li class=""><a href="#">Tortas Salgadas</a></li>
+                        <li class="active"><a href="#">Tortas Doces</a></li>
+                    </ul>
+    </div>
+    <div class="col-md-9 produtos">
+        <div class="row">
+                                    <div class="col-sm-4 produto">
+                <a href="/loja-virtual/produto/tortas-doces/decoracao-01">
+                    <div class="thumbnail">
+                        <div class="caption hidden-xs hidden-sm" style="display: none;">
+                            <p>Comprar</p>
+                        </div>
+                        <img src="//www.pidia.com.br/imgs/dolcegusto/produtos/377/tb_1jpg" alt="" class="img-responsive"/>
+                    </div>
+                </a>
+                <h2>Decora&ccedil;&atilde;o 01</h2>
+                                                <h3>R$ 10,00</h3>
+                                                <a href="/loja-virtual/produto/tortas-doces/decoracao-01" class="btn btn-default btn-comprar">Comprar</a>
+            </div>
+</main>
+	    <div class="loja-rodape">
+	    	<div class="container">
+	    		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+	    			<h2>Minha Conta</h2>
+	    			<ul>
+	    				<li><a href="#">Login</a></li>
+	    				<li><a href="#">N&atilde;o sou cadastrado</a></li>
+	    			</ul>
+	    		</div>
+	    		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6 col-xs-12">
+	    			<ul>
+	    				<li><a href="/#contato">Contato</a></li>
+	    			</ul>
+	    		</div>
+	    		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+	    			<h2>Formas de pagamento</h2>
+	    			<img src="/img/cielo.png" class="img-responsive espaco-cielo" />
+	    		</div>
+	    		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 redes-sociais">
+	    			<h2>Redes Sociais</h2>
+	    			<ul>
+	    				<li><a href="https://www.facebook.com/Delicias-Dona-Flor-2043568992395960" target="_blank"><i class="fa fa-facebook"></i></a></li>
+	    			</ul>
+	    			<div class="clearfix"></div>
+	    			<h2 class="mt-10">Atendimento</h2>
+	    			<div class="fone">
+	    				<a href="tel:+555430553366">54 3055-3366</a>
+    				</div>
+	    			<a href="mailto:felipe.marins.batista2003@gmail.com">Email test</a>
+	    		</div>
+	    		<div class="clearfix"></div>
+	    		<div class="col-lg-12 text-center logo">
+	    			<a href="/">
+                        <img class="img-responsive" src="" width="100">
+	    			</a>
+	    		</div>
+	    	</div>
+	    </div>
+    </div>
+  </div>
+</div>
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1626959264262794');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1626959264262794&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+
+	</body>
 </html>
