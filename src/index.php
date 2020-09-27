a6b0ef63401dbcf942d6d19a73c328f680629c7b
Luis Felipe Marins Batista <felipe.marins.batista2003@gmail.com>
2020-09-27 16:08:48 -0300

    Index Alteração

--- src/index.php
+++ src/index.php
@@ -1,14 +1,3 @@
-<?php
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["login"]) || $_SESSION["login"] !== true){
-    header("location: login.php");
-    exit;
-}
-?>
- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
