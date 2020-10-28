f1c34f2fdf7a7a82f2ddc64c035a5be6ba41462d
Luis Felipe Marins Batista <felipe.marins.batista2003@gmail.com>
2020-10-28 13:50:51 -0300

    Navbar

--- src/index.php
+++ src/index.php
@@ -9,13 +9,19 @@
 
           <title>Tortas Doces - Loja Virtual Dolce Gusto</title>
 
-        <link media="all" type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans:400,700">
+          <link media="all" type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans:400,700">
 
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link media="all" type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-    <link media="all" type="text/css" rel="stylesheet" href="main.css">
+<link media="all" type="text/css" rel="stylesheet" href="https://www.dolcegusto.com.br/css/bootstrap.min.css">
 
-    <link media="all" type="text/css" rel="stylesheet" href="interface.css">
+<link media="all" type="text/css" rel="stylesheet" href="https://www.dolcegusto.com.br/css/bootstrap-select.min.css">
+
+<link media="all" type="text/css" rel="stylesheet" href="https://www.dolcegusto.com.br/css/bootstrap-datetimepicker.min.css">
+
+<link media="all" type="text/css" rel="stylesheet" href="https://www.dolcegusto.com.br/build/css/main-19f5978c25.css">
+
+<link media="all" type="text/css" rel="stylesheet" href="https://www.dolcegusto.com.br/build/css/loja-virtual-4088763c98.css">
 
   </head>
 
