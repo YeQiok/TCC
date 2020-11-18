309a7c0c13415611bb7c535ab4fc77f5a947226a
YeQioK <felipe.marins.batista2003@gmail.com>
2020-11-18 19:53:10 -0300

    Add Css

--- src/index.php
+++ src/index.php
@@ -10,8 +10,8 @@
   <title>Torta DonaFlor</title>
 
   <link media="all" type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans:400,700">
-  <link rel="stylesheet" href="main.css">
-  <link rel="stylesheet" href="interface.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/interface.css">
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
   <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
