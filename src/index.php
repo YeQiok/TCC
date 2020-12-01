f837580cd87b37cc1120528a398f090d4c64ac08
YeQioK <felipe.marins.batista2003@gmail.com>
2020-12-01 09:21:50 -0300

    index

--- src/index.php
+++ src/index.php
@@ -1,244 +1,243 @@
+
 <html lang="pt-br">
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="initial-scale=1, width=device-width">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="initial-scale=1, width=device-width">
 
-  <title>Dona Flor</title>
+    <title>Dona Flor</title>
 
-  <link media="all" type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans:400,700">
+    <link media="all" type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script|Open+Sans:400,700">
 
-  <link media="all" type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-  <link media="all" type="text/css" rel="stylesheet" href="./css/bootstrap.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="./css/bootstrap.min.css">
 
-  <link media="all" type="text/css" rel="stylesheet" href="./css/bootstrap-select.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="./css/bootstrap-select.min.css">
 
-  <link media="all" type="text/css" rel="stylesheet" href="./css/bootstrap-datetimepicker.min.css">
+    <link media="all" type="text/css" rel="stylesheet" href="./css/bootstrap-datetimepicker.min.css">
 
-  <link media="all" type="text/css" rel="stylesheet" href="./css/main.css">
+    <link media="all" type="text/css" rel="stylesheet" href="./css/main.css">
 
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 
-  <link media="all" type="text/css" rel="stylesheet" href="./css/interface.css">
+    <link media="all" type="text/css" rel="stylesheet" href="./css/interface.css">
 
-  <script type="text/javascript" src="js/main.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
 
-  <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="shortcut icon" href="img/favicon.ico">
 </head>
 <header id="tudo">
-  <main class="content">
+    <main class="content">
     <nav id="menu-inicial" class="navbar navbar-expand-lg">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         <img src=";/img/logo.png">
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
-          <li class="nav-item active">
+        <li class="nav-item active">
             <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="#">Features</a>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="#">Pricing</a>
-          </li>
+        </li>
         </ul>
-      </div>
+    </div>
     </nav>
     <!-- Carousel -->
     <div class="col-100">
-      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="./img/Banner1.jpg" class="d-block w-100" alt="..." height="500px">
-          </div>
-          <div class="carousel-item">
-            <img src="./img/Banner2.jpg" class="d-block w-100" alt="..." height="500px">
-          </div>
-          <div class="carousel-item">
-            <img src="./img/Banner3.jpg" class="d-block w-100" alt="..." height="500px">
-          </div>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="./img/Banner1.jpg" class="d-block w-100" alt="..." height="500px">
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+        <div class="carousel-item">
+          <img src="./img/Banner2.jpg" class="d-block w-100" alt="..." height="500px">
+        </div>
+        <div class="carousel-item">
+          <img src="./img/Banner3.jpg" class="d-block w-100" alt="..." height="500px">
+        </div>
       </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
     </div>
-    <!-- Mensagem no meio da tela -->
-    <div class="listrado" id="ancora" rel="0">
-      <h1>
+</div>
+<!-- Mensagem no meio da tela -->
+<div class="listrado" id="ancora" rel="0">
+    <h1>
         <div class="t2">Compre Sua torta</div>
         <div class="t1">Frete grátis nas compras</div>
         <div class="t3">Pague em até 3 vezes s/ juros no cartão</div>
-      </h1>
-    </div>
-    <div class="container">
-      <div class="col-md-3 menu">
-        <h1>Produtos</h1>
-        <ul>
-          <li class=""><a href="/loja-virtual/produtos/tortas-salgadas#ancora">Tortas Salgadas</a></li>
-          <li class="active"><a href="/loja-virtual/produtos/tortas-doces#ancora">Tortas Doces</a></li>
-        </ul>
-      </div>
-      <div class="card" style="width: 18rem;">
-        <img src="./img/torta.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Torta Banoffee</h5><br /></br />
-          <a id="negrito">R$ 50,00</a>
-          <p class="card-text"> em até 3x 16,67 no cartão</p><br />
-          <a href="#" class="btn btn-primary">Comprar</a>
-        </div>
-      </div>
-      <div id="card">
-        <div class="card" style="width: 18rem;">
-          <img src="./img/torta.jpg" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Torta Cheesecake de Amora</h5><br />
-            <a id="negrito">R$ 74,00</a>
-            <p class="card-text"> em até 3x 24,67 no cartão</p><br />
-            <a href="#" class="btn btn-primary">Comprar</a>
-          </div>
-        </div>
-      </div>
-      <div class="card" style="width: 18rem;">
-        <img src="./img/torta.jpg" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Torta Cheesecake de Frutas Vermelhas</h5><br />
-          <a id="negrito">R$ 74,00</a>
-          <p class="card-text">em até 3x 24,67 no cartão</p><br />
-          <a href="#" class="btn btn-primary">Comprar</a>
-        </div>
-      </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    </div>
-  </main>
-  <!-- Footer -->
-  <footer id="footer" class="page-footer font-small stylish-color-dark pt-4">
+    </h1>
+</div>
+        <div class="container">
+            <div id="produtos" class="col-md-12 produtos">
+                <div class="row">
+                    <div class="col-md-3 menu">
+                        <h1>Produtos</h1>
+                        <ul>
+                            <li class=""><a href="#">Tortas Salgadas</a></li>
+                            <li class="active"><a href="#">Tortas Doces</a></li>
+                        </ul>
+                </div>
+                <div class="card" style="width: 18rem;">
+                    <img src="./img/torta.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Torta Banoffee</h5><br /></br />
+                            <a id="negrito">R$ 50,00</a><p class="card-text"> em até 3x 16,67 no cartão</p><br />
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                        </div>
+                </div>
+                <div id="card">
+                    <div class="card" style="width: 18rem;">
+                        <img src="./img/torta.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Torta Cheesecake de Amora</h5><br />
+                                <a id="negrito">R$ 74,00</a><p class="card-text">  em até 3x 24,67 no cartão</p><br />
+                                <a href="#" class="btn btn-primary">Comprar</a>
+                            </div>
+                    </div>
+                </div>     
+                    <div class="card" style="width: 18rem;">
+                    <img src="./img/torta.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Torta Cheesecake de Frutas Vermelhas</h5><br />
+                            <a id="negrito">R$ 74,00</a><p class="card-text">em até 3x 24,67 no cartão</p><br />
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                        </div>
+                </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+    </main>
+    <!-- Footer -->
+<footer id="footer" class="page-footer font-small stylish-color-dark pt-4">
 
-    <!-- Footer Links -->
-    <div class="container text-center text-md-left">
+<!-- Footer Links -->
+<div class="container text-center text-md-left">
 
-      <!-- Grid row -->
-      <div class="row">
+  <!-- Grid row -->
+  <div class="row">
 
-        <!-- Grid column -->
-        <div class="col-md- mx-auto">
-
-        </div>
-        <!-- Grid column -->
-
-        <hr class="clearfix w-100 d-md-none">
-
-        <!-- Grid column -->
-        <div class="col-md-2 mx-auto">
-
-          <!-- Links -->
-          <h5 id="footer-text" class="font-weight-bold text-uppercase mt-3 mb-4 ">Minha Conta</h5>
-
-          <ul class="list-unstyled">
-            <li>
-              <a id="footer-text" href="#!">login</a>
-            </li>
-            <li>
-              <a id="footer-text" href="#!">Registre-se</a>
-            </li>
-          </ul>
-        </div>
-        <!-- Grid column -->
-
-        <hr class="clearfix w-100 d-md-none">
-
-        <!-- Grid column -->
-        <div class="col-md-2 mx-auto">
-
-          <!-- Links -->
-          <h5 id="footer-text" class="font-weight-bold text-uppercase mt-3 mb-4">Formas de Pagamento</h5>
-
-          <ul class="list-unstyled">
-            <li>
-              <a id="footer-text"><img src="img/cielo.png"></a>
-            </li>
-          </ul>
-
-        </div>
-        <!-- Grid column -->
-
-        <hr class="clearfix w-100 d-md-none">
-
-        <!-- Grid column -->
-        <div class="col-md-2 mx-auto">
-
-          <!-- Links -->
-          <h5 id="footer-text" class="font-weight-bold text-uppercase mt-3 mb-4">Redes Sociais</h5>
-
-          <ul class="list-unstyled">
-            <li>
-              <a id="footer-text" href="#!">Facebook</a>
-            </li>
-          </ul>
-
-        </div>
-        <!-- Grid column -->
-
-      </div>
-      <!-- Grid row -->
+    <!-- Grid column -->
+    <div class="col-md- mx-auto">
 
     </div>
-    <div id="footer-2">
-      <hr>
-      <!-- Social buttons -->
-      <ul class="list-unstyled list-inline text-center">
-        <li class="list-inline-item">
-          <a class="btn-floating btn-fb mx-1">
-            <i class="fab fa-facebook-f"> </i>
-          </a>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 mx-auto">
+
+      <!-- Links -->
+      <h5 id="footer-text"  class="font-weight-bold text-uppercase mt-3 mb-4 ">Minha Conta</h5>
+
+    <ul class="list-unstyled">
+        <li>
+          <a id="footer-text" href="#!">login</a>
         </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-tw mx-1">
-            <i class="fab fa-twitter"> </i>
-          </a>
+        <li>
+          <a id="footer-text" href="#!">Registre-se</a>
         </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-gplus mx-1">
-            <i class="fab fa-whatsapp"></i>
-          </a>
-        </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-li mx-1">
-            <i class="fab fa-linkedin-in"> </i>
-          </a>
-        </li>
-        <li class="list-inline-item">
-          <a class="btn-floating btn-dribbble mx-1">
-            <i class="fab fa-dribbble"> </i>
-          </a>
+    </ul>
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 mx-auto">
+
+      <!-- Links -->
+      <h5 id="footer-text" class="font-weight-bold text-uppercase mt-3 mb-4">Formas de Pagamento</h5>
+
+      <ul class="list-unstyled">
+        <li>
+          <a id="footer-text"><img src="img/cielo.png"></a>
         </li>
       </ul>
-      <!-- Social buttons -->
-  </footer>
+
+    </div>
+    <!-- Grid column -->
+
+    <hr class="clearfix w-100 d-md-none">
+
+    <!-- Grid column -->
+    <div class="col-md-2 mx-auto">
+
+      <!-- Links -->
+      <h5 id="footer-text" class="font-weight-bold text-uppercase mt-3 mb-4">Redes Sociais</h5>
+
+      <ul class="list-unstyled">
+        <li>
+          <a  id="footer-text" href="#!">Facebook</a>
+        </li>
+      </ul>
+
+    </div>
+    <!-- Grid column -->
+
   </div>
-  <!-- Footer -->
+  <!-- Grid row -->
+
+</div>
+<div id="footer-2">
+<hr>
+<!-- Social buttons -->
+<ul class="list-unstyled list-inline text-center">
+    <li class="list-inline-item">
+      <a class="btn-floating btn-fb mx-1">
+        <i class="fab fa-facebook-f"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-tw mx-1">
+        <i class="fab fa-twitter"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-gplus mx-1">
+      <i class="fab fa-whatsapp"></i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-li mx-1">
+        <i class="fab fa-linkedin-in"> </i>
+      </a>
+    </li>
+    <li class="list-inline-item">
+      <a class="btn-floating btn-dribbble mx-1">
+        <i class="fab fa-dribbble"> </i>
+      </a>
+    </li>
+  </ul>
+<!-- Social buttons -->
+</footer>
+</div>
+<!-- Footer -->
 </header>
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 </body>
-
 </html>
